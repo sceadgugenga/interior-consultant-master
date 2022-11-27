05b2fb976e0b3b72011a8f33169558501200a8ad
Sceadgugenga <sceadgugenga@gmail.com>
2022-11-27 12:07:14 -0800

    Fixed menu and updated css

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
 let hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("click", toggleMenu, false);
-// window.addEventListener('resize', checkWindowSize);
+window.addEventListener("resize", checkWindowSize);
 
+let newBreakpoint = true;
 
-// function checkWindowSize() {
-// const windowWidth = window.innerWidth;
-// if (windowWidth > 600) {
-//     let mainNav = document.getElementById("main-nav");
-//     mainNav.classList.remove("menu-open")
-//     mainNav.classList.remove("menu-closed")
-//     // hamburger.classList.remove("menu-open-icon")
-// }
+function checkWindowSize() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 900) {
+    if (newBreakpoint) {
+      let mainNav = document.getElementById("main-nav");
+      mainNav.classList.remove("menu-open");
 
-// }
-
-function toggleMenu() {
-    let mainNav = document.getElementById("main-nav");
-    
-    let mainNavClassList = mainNav.className
-    console.log(mainNav.style.display)
-    // console.log(mainNavClassList.indexOf("menu-slidein"))
-    // if (mainNav.style.display === "flex") {
-    if (mainNavClassList.indexOf("menu-open") > -1) {
-        mainNav.classList.remove("menu-open")
-        hamburger.classList.remove("menu-open-icon")
-        mainNav.classList.add("menu-closed")
-        hamburger.classList.add("menu-closed-icon")
-      
-        // mainNav.style.display = "none"
+      hamburger.classList.remove("menu-open-icon");
+      newBreakpoint = false;
     } else {
-            mainNav.classList.remove("menu-closed")
-        hamburger.classList.remove("menu-closed-icon")
-        mainNav.classList.add("menu-open")
-        hamburger.classList.add("menu-open-icon")
-        // mainNav.style.display = "flex";
+      newBreakpoint = true;
     }
   }
+}
+
+function toggleMenu() {
+  let mainNav = document.getElementById("main-nav");
+
+  let mainNavClassList = mainNav.className;
+  mainNav.classList.toggle("menu-open");
+  hamburger.classList.toggle("menu-open-icon");
+}
